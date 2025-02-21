77ebd0bd3bb4dc40dfadcf18e125fd6f7cdb1097
djarwoso <jarwoso@gmail.com>
2025-02-21 13:54:43 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+
+</head>
+<body>
+    <h1>Hello Word</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, doloremque reprehenderit, aut quod tempore nostrum animi placeat voluptatem esse odio ad corrupti blanditiis iste, perferendis aspernatur aliquid praesentium porro deserunt!</p>
+
+    
+</body>
+</html>
